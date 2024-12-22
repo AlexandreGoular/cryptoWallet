12fc9614f593005444b9289a9d30e8a6d6905a49
AlexandreGoular <xandeaugustogoulart@gmail.com>
2024-12-22 17:29:58 -0300

    Create MiningTypes CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :mining_types
   get 'cryptoWallet', to: 'welcome#index'
   resources :coins
   get "up" => "rails/health#show", as: :rails_health_check
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_16_215002) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_22_202748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_16_215002) do
     t.string "description"
     t.string "acronym"
     t.string "url_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mining_types", force: :cascade do |t|
+    t.string "name"
+    t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
